d0e4e61b06feea579579a431b462adc7e6094b26
Vetrano89 <crvetrano@gmail.com>
2022-02-11 00:54:14 -0500

    add view engine back

--- app.js
+++ app.js
@@ -9,13 +9,16 @@ var indexRouter = require('./routes/index');
 var itemsRouter = require('./routes/items');
 var app = express();
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
@@ -24,10 +27,6 @@ app.use('/items', itemsRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 // error handler
